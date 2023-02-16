b8c9e86b254ec79e4462c28fd503d08e678d663b
BeepIsla <BeepIsla@gmail.com>
2023-02-16 04:04:08 +0100

    Update

--- README.md
+++ README.md
@@ -2,42 +2,8 @@
 
 Easy and uniform way to compile our plugins.
 
-This is my first Github Action I've ever made, its not pretty but it works and allows us to change compile settings without having to update all of our plugins. Things here may change at any time without notice, this is only really for internal usage.
+This is my first Github Action I've ever made, its not pretty but it works and allows us to change compile settings without having to update all of our plugins.
 
-## Example
+The compilation takes advantage of our custom installation and distribution system's `install.json` to download dependencies and handle configuration. Due to this there is no official example or support.
 
-Read `actions.yml` for more information regarding inputs and to learn about which inputs are optional and default values.
-
-```yml
-name: build
-
-on:
-    push:
-    pull_request:
-    workflow_dispatch:
-
-jobs:
-    build:
-        name: build
-        runs-on: ubuntu-latest
-
-        steps:
-            - name: Checkout
-              uses: actions/checkout@v3
-
-            - name: Compile
-              uses: CasualTF2/sp-compile@main
-              with:
-                  source-files: |
-                      src/file1.sp
-                      src/file2.sp
-                      src/file3.sp
-                  include-paths: |
-                      some/other/folder1
-                      some/other/folder2
-                      some/other/folder3
-                  output-path: output
-
-            - name: Do something with the files
-              run: ls output
-```
+This action is open source to more easily be able to use it in our projects, because private actions are a pain in the ass.
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as childProcess from "child_process";
-import glob from "glob";
 
 const basePath = process.argv.slice(2).join(" ");
-/** @type {string[]} */
-const includePaths = fs
-	.readFileSync("INCLUDE_PATHS.txt", "utf8")
-	.split("\n")
-	.map((l) => path.join(basePath, l.replace(/\r/g, "").trim()))
-	.filter((l) => l.length > 0);
 /** @type {string[]} */
 const sourceFiles = fs
 	.readFileSync("SOURCE_FILES.txt", "utf8")
 	.split("\n")
-	.map((l) => l.replace(/\r/g, "").trim())
+	.map((l) => path.join(basePath, l.replace(/\r/g, "").trim()))
 	.filter((l) => l.length > 0)
-	.map((line) => glob.sync(path.join(basePath, line), { nodir: true, absolute: true }))
 	.flat()
 	.filter((l) => typeof l === "string" && l.endsWith(".sp"));
+/** @type {string[]} */
+const extraIncludePaths = fs
+	.readFileSync("EXTRA_INCLUDE_PATHS.txt", "utf8")
+	.split("\n")
+	.map((l) => path.join(basePath, l.replace(/\r/g, "").trim()))
+	.filter((l) => l.length > 0);
 let outputPath = fs.readFileSync("OUTPUT_PATH.txt", "utf8").replace(/[\r\n]/g, "");
 if (outputPath.length <= 0) {
 	outputPath = undefined;
@@ -32,6 +30,53 @@ if (outputPath.length <= 0) {
 	});
 }
 
+let installJsonPath = fs.existsSync("INSTALL_JSON_PATH_OVERRIDE.txt")
+	? fs.readFileSync("INSTALL_JSON_PATH_OVERRIDE.txt", "utf8").replace(/\n|\r/g, "").trim()
+	: undefined;
+if (!installJsonPath || installJsonPath.length <= 0) {
+	installJsonPath = path.join(basePath, "install.json");
+}
+
+// Parse "install.json", download any dependencies, and assemble include paths
+const json = JSON.parse(fs.readFileSync(installJsonPath));
+const includePaths = [...extraIncludePaths, ...(json?.inclusion ?? []).map((i) => path.join(basePath, i))];
+function downloadDependencies(dependencies) {
+	if (!Array.isArray(dependencies)) {
+		return;
+	}
+
+	for (const dependency of dependencies) {
+		// Don't download dependencies we already downloaded
+		const dependencyPath = path.join(process.cwd(), dependency.repo);
+		if (fs.existsSync(dependencyPath)) {
+			continue;
+		}
+
+		let url = `https://github.com/${dependency.user}/${dependency.repo}`;
+		if (process.env.GITHUB_ACCESS_TOKEN) {
+			url = `https://${dependecy.user}:${process.env.GITHUB_ACCESS_TOKEN}@github.com/${dependency.user}/${dependency.repo}`;
+		}
+
+		childProcess.execSync(`git clone ${url} ${dependencyPath}`, {
+			cwd: process.cwd(),
+			stdio: "inherit",
+			encoding: "utf-8"
+		});
+
+		// Parse its install.json and add include paths
+		const json = JSON.parse(fs.readFileSync(path.join(dependencyPath, "install.json")));
+		if (json.inclusion) {
+			for (const include of json.inclusion) {
+				includePaths.push(path.join(dependencyPath, include));
+			}
+		}
+
+		// Download dependencies recursively
+		downloadDependencies(json.dependencies);
+	}
+}
+downloadDependencies(json.dependencies);
+
 console.log(`Include paths: ${includePaths.join(", ")}`);
 
 for (const file of sourceFiles) {
@@ -41,20 +86,20 @@ for (const file of sourceFiles) {
 
 	childProcess.execSync(
 		[
-			"spcomp",
+			"spcomp", // When running locally for testing ensure spcomp is in your PATH
 			file, // Input file
 			"-E", // Treat warnings as errors
 			"-O2", // Optimization level (0=none, 2=full)
 			"-v2", // Verbosity level; 0=quiet, 1=normal, 2=verbose
-			`-i${process.env.includePath}`, // SourcePawn include path from setup-sp
-			includePaths.map((i) => `-i${i}`), // INclude paths
+			process.env.includePath ? `-i${process.env.includePath}` : [], // SourcePawn include path from setup-sp (Does not exist locally)
+			includePaths.map((i) => `-i${i}`), // Include paths
 			`-o ${outputPath || parsed.dir}/${parsed.base.replace(".sp", ".smx")}` // Output path, either from input or same as source file
 		]
 			.flat()
 			.join(" "),
 		{
 			encoding: "utf8",
-			stdio: "pipe"
+			stdio: "inherit"
 		}
 	);
 }
